87409742efeffce22e96c58d4c15ec63e07b676d
valeriavorobej <vorobalapota@icloud.com>
2023-11-24 11:31:10 +0300

    <3

--- src/ru/mirea/lab19/n1/Shopping.java
+++ src/ru/mirea/lab19/n1/Shopping.java
@@ -29,33 +29,7 @@ public class Shopping {
         }
     }
 }
-//    public static void main(String [] args){
-//        Scanner in = new Scanner(System.in);
-//        System.out.print("Имя: ");
-//        String name = in.next();
-//        System.out.print("Фамилия: ");
-//        String surname = in.next();
-//        System.out.println("Введите ИНН: ");
-//        String INN = in.next();
-//
-//        try {
-//
-////            System.out.println("Введите ИНН: ");
-////            String INN = in.next();
-////            int i = Integer.parseInt(INN);
-//
-//        }
-//        catch(WrongINN err)
-//        {
-//            if()
-//            {
-//                throw new WrongINN("Неправильный ИНН " + INN, err);
-//            }
-//        }
-//
-//        System.out.println(name);
-//        System.out.println(surname);
-//    }
+
 
 
 
